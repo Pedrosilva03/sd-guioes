2ddc5f4027382f8ac76b07522cb544d2bfb2cd5e
Pedrosilva03 <pedroo.silva180@gmail.com>
2024-07-09 13:11:56 +0100

    Modificação do servidor para o guião 6

--- guiao6/EchoServer.java
+++ guiao6/EchoServer.java
@@ -6,9 +6,13 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class EchoServer {
 
+    static int soma = 0;
+    static int counter = 0;
+    static ReentrantLock lock = new ReentrantLock();
     public static void main(String[] args) {
         try {
             ServerSocket ss = new ServerSocket(12345);
@@ -19,18 +23,40 @@ public class EchoServer {
                 BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
                 PrintWriter out = new PrintWriter(socket.getOutputStream());
 
-                String line;
-                while ((line = in.readLine()) != null) {
-                    out.println(line);
-                    out.flush();
-                }
+                Thread t = new Thread(() -> {
+                    try{
+                        String line;
+                        while ((line = in.readLine()) != null) {
+                            if(line.equals("")){
+                                out.println(soma / counter);
+                                out.flush();
+                            }
+                            else{
+                                soma(Integer.parseInt(line));
+                                out.println(soma);
+                                out.flush();
+                            }
+                        }
 
-                socket.shutdownOutput();
-                socket.shutdownInput();
-                socket.close();
+                        socket.shutdownOutput();
+                        socket.shutdownInput();
+                        socket.close();
+                    }
+                    catch(Exception e){
+                        e.printStackTrace();
+                    }
+                });
+                t.start();
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    public static void soma(int line){
+        lock.lock();
+        soma += line;
+        counter++;
+        lock.unlock();
     }
 }
