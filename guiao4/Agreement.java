dd6738f27b61e6ed37050a14048556e71b1faa8c
Pedrosilva03 <pedroo.silva180@gmail.com>
2024-07-11 12:04:57 +0100

    Mudança no guião 4 para usar a barrier

--- guiao4/Agreement.java
+++ guiao4/Agreement.java
@@ -5,50 +5,31 @@ import java.util.concurrent.locks.ReentrantLock;
 
 public class Agreement {
     private int N;
-    private int counter1;
-    private int counter2;
     private int max;
     private ReentrantLock lock;
-    private Condition cond1;
-    private Condition cond2;
+    private Barrier b1;
+    private Barrier b2;
     public Agreement(int N){
         this.N = N;
-        this.counter1 = 0;
-        this.counter2 = 0;
         this.max = 0;
         this.lock = new ReentrantLock();
-        this.cond1 = lock.newCondition();
-        this.cond2 = lock.newCondition();
+        this.b1 = new Barrier(this.N);
+        this.b2 = new Barrier(this.N);
     }
 
     public int propose(int choice) throws InterruptedException{
-        this.lock.lock();
-        this.counter1++;
-        this.cond1.signalAll();
+        b1.await();
 
-        while(this.counter1 < this.N){
-            this.cond1.await();
-        }
-
-        this.lock.unlock();
         this.lock.lock();
 
         if(choice > this.max){
             this.max = choice;
         }
 
-        this.counter2++;
-        this.cond2.signalAll();
+        this.lock.unlock();
 
-        while(this.counter2 < this.N){
-            this.cond2.await();
-        }
+        b2.await();
 
-        try{
-            return this.max;
-        }
-        finally{
-            this.lock.unlock();
-        }
+        return this.max;
     }
 }
--- guiao4/Main.java
+++ guiao4/Main.java
@@ -7,14 +7,12 @@ public class Main {
         int N = 10;
 
         Agreement a = new Agreement(N);
-        Barrier b = new Barrier(N);
 
         Random r = new Random();
 
         for(int i = 0; i < N; i++){
             Thread t = new Thread(() -> {
                 try{
-                    //b.await();
                     System.out.println(a.propose(r.nextInt()));
                 }
                 catch(InterruptedException e){
